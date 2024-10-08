e2211d7e2a2078b28861a65045a12fffbe809b9e
ramsey <mungairamsey01@gmail.com>
2024-10-08 14:00:01 +0300

    data encryption, id generation and user creation completed

--- user.py
+++ user.py
@@ -1,5 +1,9 @@
 from validator_collection import checkers
-import password
+import os
+from cryptography.fernet import Fernet
+import csv
+
+
 
 class User:
 
@@ -18,7 +22,8 @@ class User:
         self.name = self.input_name()
         self.email = self.input_email()
         self.password = self.input_password()
-        self.add_user()
+        self._UID = self.UID_generator()
+        self.encrypt_data()
 
     def user_logins():
         self.name = self.input_name()
@@ -32,27 +37,60 @@ class User:
             if name:
                 return name
     
+
     def input_email(self):
         while True:
             email = input("Email: ")
             if checkers.is_email(email):
                 return email
     
+
     def input_password(self):
         password = input("Password: ")
         return password
+    
 
-    def add_user():
-        ... to do
-        #usr details to be appended to the csv
-        #generate user id
-        #how to implement api tokens in normal python apps
-        #hot to store user credentials.
+    def encrypt_data(self):
+        #this function encrypts all data to be stored in the new users csv
+        key = Fernet.generate_key()
+        self.fernet = Fernet(key)
+        self.usertoken = fernet.encrypt(self.name)
+        self.emailtoken = fernet.encrypt(self.email)
+        self.passwordtoken = fernet.encrypt(self.password)
+        self.add_user()
+
+    def decrypt_data(self):
+        #this function decrypts data in the csv to authenticate user logins
+
+    def add_user(self):
+        #here we add users to a csv file to help with local authentication 
+        csv_file_path = "Users.csv"
+        header = ["username", "email", "password", "userID"]
+        
+        #checking if csv file exists and if its empty
+        if not os.path.exists(csv_file_path) or os.path.getsize(csv_file_path) == 0:
+            with open(csv_file_path, mode = 'w', newline= '') as file:
+                writer = csv.DictWriter(file, fieldnames=header)
+                writer.writeheader()
+
+        #appending to the csv file using dicts
+        with open(csv_file_path, mode='a', newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=header)
+            writer.writerow({"username": self.usertoken, "email":self.emailtoken, "password"=self.passwordtoken, "userID": self._UID
+
 
     def validate_credentials(self):
         #import the users csv or check api
         #check if usrname in csv
         #pick usrid to do all other functions
+
+
+    def UID_generator():
+        #this function generates 10 character long user ids
+        #i use this to identify user sessions 
+        return shortuuid.uuid(length=10)
+        
+
 
 class User_Account():
     def __init__(self,uid):
