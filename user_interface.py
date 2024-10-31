7f17ad09907b33f1cf70ba72a424c09104ec0354
Ramsey <mungairamsey01@gmail.com>
2024-10-31 23:00:55 +0300

    started user interface

--- user_interface.py
+++ user_interface.py
@@ -1,6 +1,40 @@
 import tkinter as tk
 import customtkinter as ctk
 
-class UserInterface:
+
+
+ctk.set_appearance_mode("system")
+ctk.set_default_color_theme("dark-blue")
+
+class App(ctk.CTk):
     def __init__(self):
-        pass
+        super().__init__()
+
+        self.title('BusQr')
+        self.geometry('600*500')
+
+        self.bus_button = ctk.CTkButton(self, text='Bus' )
+        self.bus_button.pack(pady=20, padx=20)
+        self.user_button = ctk.CTkButton(self, text=  'User', command = self.launch_user )
+        self.user_button.pack(pady=20, padx=20)
+
+    def launch_user(self):
+        self.user_button.destroy()
+        self.bus_button.destroy()
+        #label welcomes users
+        self.welcome_label = ctk.CTkLabel(self, text='Karibu User')
+        self.welcome_label.pack(pady=20)
+        self.login_button = ctk.CTkButton(self, text='Login' ,command = self.login)
+        self.login_button.grid(row=0, column=0, padx=10)
+
+        self.register_button = ctk.CTkButton(self, text= 'Register', command = self.register)
+        self.register_button.grid(row=0, column=1, padx=10)
+
+
+
+
+
+
+
+app = App()
+app.mainloop()
